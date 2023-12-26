1488f98a03363ac7a49b655d95effeee8cfd7e8b
revokim <revokim@shiftup.co.kr>
2023-12-26 23:53:52 +0900

    1000-1001

---
+++ Baekjoon/CLASS/1/1001.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+// 문제
+//     두 정수 A와 B를 입력받은 다음, A-B를 출력하는 프로그램을 작성하시오.
+//
+//     입력
+//     첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+//
+// 출력
+//     첫째 줄에 A-B를 출력한다.
+
+namespace baekjoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] s = Console.ReadLine().Split();
+
+            Console.WriteLine(int.Parse(s[0]) - int.Parse(s[1]));
+        }
+    }
+}
